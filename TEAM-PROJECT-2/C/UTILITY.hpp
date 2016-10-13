e3aaf0abc9cb97289e85571c66d5d277cae62f8d
JoryLiu <liuzhaorui55@hotmail.com>
2016-10-13 10:28:47 +0800

    Create UTILITY.hpp

---
+++ TEAM-PROJECT-2/C/UTILITY.hpp
@@ -0,0 +1,12 @@
+#ifndef UTILITY_HPP_
+#define UTILITY_HPP_
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <ctype.h>
+#include <time.h>
+ 
+enum Error_code { success, fail, range_error, underflow, overflow, fatal,
+                  not_present, duplicate_error, entry_inserted, entry_found,
+                  internal_error };
+#endif
