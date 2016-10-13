e50f24a434d9923e843e6fca7a3caf65b1ecf71e
JoryLiu <liuzhaorui55@hotmail.com>
2016-10-13 10:33:30 +0800

    Create QUEUE.hpp

---
+++ TEAM-PROJECT-2/3/QUEUE/QUEUE.hpp
@@ -0,0 +1,20 @@
+#ifndef QUEUE_HPP_
+#define QUEUE_HPP_
+#include "../../C/UTILITY.hpp"
+
+const int maxqueue = 10; //  small value for testing
+
+template <class Queue_entry>
+class Queue {
+public:
+   Queue();
+   bool empty() const;
+   Error_code serve();
+   Error_code append(const Queue_entry &item);
+   Error_code retrieve(Queue_entry &item) const;
+protected:
+   int count;
+   int front, rear;
+   Queue_entry entry[maxqueue];
+};
+#endif
