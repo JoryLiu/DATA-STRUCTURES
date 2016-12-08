da213fc212bc22ed16ec46b5d97f1a4af45d2103
JoryLiu <liuzhaorui55@hotmail.com>
2016-12-08 16:41:47 +0800

    Update Topological-sorting.cpp

--- Topological-sorting.cpp
+++ Topological-sorting.cpp
@@ -1,10 +1,12 @@
 #include <cstdio>
 #include <vector>
+#include <algorithm>
+#include <set>
 using namespace std;
 
 struct node {
     int index;
-    vector<int> precursor;
+    vector<int> successor;
     node(int i) : index(i) {}
 };
 
@@ -13,33 +15,33 @@ int main() {
     scanf("%d", &t);
     while (t--) {
         int n, m, index, pre, i, j;
-        node* root;
         scanf("%d%d", &n, &m);
         vector<node*> v;
-        vector<bool> isFinished(n + 1, false);
-        bool hasPre;
+        vector<int> precursor(200000, 0);
         for (int i = 0; i < n; i++)
             v.push_back(new node(i + 1));
         while (m--) {
             scanf("%d%d", &pre, &index);
-            v[index - 1]->precursor.push_back(pre);
+            v[pre - 1]->successor.push_back(index);
+            precursor[index]++;
         }
-        while (!v.empty()) {
-            for (i = 0; i < v.size(); i++) {
-                if (isFinished[v[i]->index]) continue;
-                hasPre = false;
-                if (!v[i]->precursor.size()) break;
-                for (j = 0; j < v[i]->precursor.size(); j++)
-                    if (!isFinished[v[i]->precursor.at(j)]) {
-                        hasPre = true;
-                        break;
-                    }
-                if (!hasPre) break;
+        set<int> s;
+        for (i = 1; i <= n; i++)
+            if (!precursor[i])
+                s.insert(i);
+        while (!s.empty()) {
+            index = *s.begin();
+            printf("%d ", index);
+            for (j = 0; j < v[index - 1]->successor.size(); j++) {
+                precursor[v[index - 1]->successor.at(j)]--;
+                if (precursor[v[index - 1]->successor.at(j)] == 0)
+                    s.insert(v[index - 1]->successor.at(j));
             }
-            printf("%d ", v[i]->index);
-            isFinished[v[i]->index] = true;
-            v.erase(v.begin() + i);
+            s.erase(index);
         }
+        for (i = 0; i < v.size(); i++) delete v[i];
+        v.clear();
+        printf("\n");
     }
     return 0;
 }
